35971e5f2f8962fe02cdb58d3d4df083873fc7e7
anaestevam <anaestevam9@gmail.com>
2023-09-18 21:21:39 -0300

    ate aula 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [SiteController::class, 'home']);
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
